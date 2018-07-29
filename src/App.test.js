db14550fd561f9611a5256514236d74674265b95
hnrqss <hnrq.assuncao@gmail.com>
2018-07-29 13:02:13 -0300

    starts with tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import Enzyme, { shallow, mount, render } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter : new Adapter()})
+
+describe('<App />', () => {
+  
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.length).toBe(1)
+  })
+  
+  it('should have .container class', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.is('.container')).toBe(true)
+  })
+
+  // it('outputs the <App />', () => {
+  //   const wrapperShallow = shallow(<App />)
+  //   const wrapperMount   = mount(<App />)
+  //   const wrapperRender  = render(<App />)
+
+  //   console.log(wrapperShallow.debug())
+  //   console.log(wrapperMount.debug())
+  //   console.log(wrapperRender.html())
+  // })
+
+})
